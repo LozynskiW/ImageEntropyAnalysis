05c853c0a320f184b8e45df9b72f76822ec37326
w.lozynski <w.lozynski@icloud.com>
2025-04-01 23:51:54 +0200

    removed broken images, added new, changed filepath

--- examples/single_file_analysis.py
+++ examples/single_file_analysis.py
@@ -1,7 +1,7 @@
 from skimage import io
 from image_processing.ready_to_use_systems import information_entropy_based_system, contour_based_system, luminance_threshold_based_system
 
-path_to_img = "C:/Users/wlozy/Pictures/wygaszacze_ekranu/wanderer-above-the-sea-of-fog-by-caspar-david-friedrich-wallpaper.jpg"
+path_to_img = "./images/thermal-imaging-humans.jpg"
 img = io.imread(path_to_img)
 
 used_system = luminance_threshold_based_system.luminance_threshold_based_system
