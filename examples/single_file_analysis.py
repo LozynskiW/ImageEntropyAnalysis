e203719e59f7883902d2320e892a97b8233a95fc
w.lozynski <w.lozynski@icloud.com>
2025-04-08 09:42:37 +0200

    added all possible options to single_file_analysis.py as comments

--- examples/single_file_analysis.py
+++ examples/single_file_analysis.py
@@ -1,7 +1,10 @@
 from skimage import io
 from image_processing.ready_to_use_systems import information_entropy_based_system, contour_based_system, luminance_threshold_based_system
 
-path_to_img = "./images/thermal-imaging-humans.jpg"
+path_to_img = "./images/thermal-imaging-boar.jpg"
+#path_to_img = "./images/thermal-imaging-humans.jpg"
+#path_to_img = "./images/thermal-imaging-deers.jpg"
+
 img = io.imread(path_to_img)
 
 used_system = luminance_threshold_based_system.luminance_threshold_based_system
