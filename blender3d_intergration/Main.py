d6f11e53a84904fb7ec48df119b7a3255f6e8a15
w.lozynski <w.lozynski@icloud.com>
2025-05-06 12:29:05 +0200

    fixed trajectories_path variable

--- blender3d_intergration/Main.py
+++ blender3d_intergration/Main.py
@@ -1,6 +1,6 @@
 from blender3d_intergration.object_trajectory_calculation.Camera import CirclePath
 
-TRAJECTORIES_PATH = './object_trajectory_calculation/calculated_trajectories'
+trajectories_path = './object_trajectory_calculation/calculated_trajectories'
 
 blender_circle_trajectory_test = CirclePath()
 blender_circle_trajectory_test.set_path_parameters(radius=10)
@@ -9,7 +9,7 @@ blender_circle_trajectory_test.calculate_trajectory()
 for h in range(20, 101, 10):
 
     blender_circle_trajectory_test.calculate_multiple_trajectories_and_save_to_script(
-        path_to_files=PATH,
+        path_to_files=trajectories_path,
         file_ext="txt",
         radius=20,
         height=h,
