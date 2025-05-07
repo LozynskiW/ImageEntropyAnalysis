fixed angular_frequency calculation

blender3d_intergration/object_trajectory_calculation/models.py
@@ -56,13 +56,15 @@ class CircularTrajectoryMotionInitialParameters:
 
 class CircularTrajectoryMotionParameters:
     __initial_parameters: CircularTrajectoryMotionInitialParameters
+    __angular_displacement_radians: float
     __time: float
     __angular_frequency: float
 
     def __init__(self, initial_parameters: CircularTrajectoryMotionInitialParameters):
         self.__initial_parameters = initial_parameters
         self.__time = self.__calculate_time(self.__initial_parameters)
-        self.__angular_frequency = self.__calculate_angular_frequency(self.__time)
+        self.__angular_frequency = self.__calculate_angular_frequency(
+            initial_parameters.angular_displacement_degrees, self.__time)
 
     def get_time(self):
         return self.__time
@@ -89,5 +91,10 @@ class CircularTrajectoryMotionParameters:
         return road / initial_parameters.linear_velocity_m_s
 
     @staticmethod
-    def __calculate_angular_frequency(time: float) -> float:
-        return 2*np.pi / time
+    def __calculate_angular_displacement_radians(angular_displacement_degrees: int) -> float:
+        return (angular_displacement_degrees * np.pi) / 180
+
+    @staticmethod
+    def __calculate_angular_frequency(angular_displacement_degrees: float, time: float) -> float:
+        angular_displacement_radians = (angular_displacement_degrees * np.pi) / 180
+        return angular_displacement_radians / time