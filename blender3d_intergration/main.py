2515fa6c7bffc1c3a2f747c7fa5fbc939fdae72a
w.lozynski <w.lozynski@icloud.com>
2025-05-22 21:45:36 +0200

    moved files, fixed main.py imports

--- blender3d_intergration/main.py
+++ blender3d_intergration/main.py
@@ -3,14 +3,18 @@ import os
 import numpy as np
 
 from blender3d_intergration.trajectories_api.trajectories_calculators.circular.calculators import SimpleCircleTrajectory
+from blender3d_intergration.trajectories_api.trajectories_calculators.circular.models import \
+    CircularTrajectoryMotionInitialParameters
 from blender3d_intergration.trajectories_api.trajectories_calculators.linear.calculators import SimpleLinearTrajectory
-from blender3d_intergration.trajectories_api.models import CoordinatesInTime, FramesPerSecond
+from blender3d_intergration.trajectories_api.models import Coordinates, FramesPerSecond
+from blender3d_intergration.trajectories_api.trajectories_calculators.linear.models import \
+    LinearTrajectoryMotionInitialParameters
 
 trajectories_path = 'trajectories_api/calculated_trajectories/new'
 script_dir = os.path.dirname(__file__)
 full_path = os.path.join(script_dir, trajectories_path)
 
-trajectory_center_point = CoordinatesInTime(0, 0, 30, 0, 0)
+trajectory_center_point = Coordinates(0, 0, 30)
 
 initial_params = CircularTrajectoryMotionInitialParameters(
     trajectory_center_point=trajectory_center_point,
@@ -24,8 +28,8 @@ circular_trajectory.calculate_trajectory()
 circular_trajectory.plot()
 
 linear_initial_params = LinearTrajectoryMotionInitialParameters(
-    start_point=CoordinatesInTime(x=10, y=0, z=30, frame=0, time_s=0),
-    end_point=CoordinatesInTime(x=100, y=0, z=30, frame=0, time_s=0),
+    start_point=Coordinates(x=10, y=0, z=30),
+    end_point=Coordinates(x=100, y=0, z=30),
     x_velocity_m_s=18*np.sqrt(2),
     y_velocity_m_s=0,
     z_velocity_m_s=0
@@ -34,4 +38,3 @@ linear_initial_params = LinearTrajectoryMotionInitialParameters(
 linear_trajectory = SimpleLinearTrajectory(linear_initial_params, FramesPerSecond.FPS_30)
 linear_trajectory.calculate_trajectory()
 linear_trajectory.plot()
-
