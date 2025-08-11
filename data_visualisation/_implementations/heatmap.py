5e0c1f57e30a2b86d27ab34b781797b6a7cf6da3
w.lozynski <w.lozynski@icloud.com>
2025-08-11 22:56:48 +0200

    major remake for heatmap

--- data_visualisation/_implementations/heatmap.py
+++ data_visualisation/_implementations/heatmap.py
@@ -1,8 +1,16 @@
+from dataclasses import dataclass
+
 import numpy as np
 from matplotlib import pyplot as plt
 
 from data_visualisation.analysis_outcome._data_for_visualisation import MultipleDatasetsValuesMap, Single3DPoint
-from data_visualisation.models import PlotOptions, FigureOptions
+from data_visualisation.models import FigureOptions
+
+
+@dataclass
+class HeatmapConfig:
+    show_cbar: bool = True
+    show_annotations: bool = True
 
 
 class Heatmap:
@@ -11,9 +19,9 @@ class Heatmap:
         self.__datasets_map = None
 
     @staticmethod
-    def plot_data(data_from_db, figure_options: FigureOptions = FigureOptions()):
+    def plot_data(data_from_db, figure_options: FigureOptions = FigureOptions(), config: HeatmapConfig = HeatmapConfig()):
 
-        datasets_map = Heatmap.__build_single_dataset_value_map(
+        datasets_map = Heatmap.__build_value_map(
             data_from_db=data_from_db,
             data_to_x_axis=figure_options.x_axis_label,
             data_to_y_axis=figure_options.y_axis_label,
@@ -27,35 +35,52 @@ class Heatmap:
             for x in np.arange(len(datasets_map.x_labels)):
                 heatmap[y][x] = datasets_map.get_point_for_x_y(datasets_map.x_labels[x],
                                                                datasets_map.y_labels[y])
+        if config.show_annotations:
+            Heatmap.__set_annotations_to_heatmap(ax, heatmap, to_percentage=True)
 
         im = ax.imshow(heatmap)
-        cbar = ax.figure.colorbar(im, ax=ax)
-        cbar.ax.set_ylabel(figure_options.z_axis_label, rotation=-90, va="bottom")
-        ax.set_xticks(np.arange(len(datasets_map.x_labels)), labels=datasets_map.x_labels)
-        ax.set_yticks(np.arange(len(datasets_map.y_labels)), labels=datasets_map.y_labels)
-        ax.set_xlabel(figure_options.x_axis_label)
-        ax.set_ylabel(figure_options.y_axis_label)
+        if config.show_cbar:
+            cbar = ax.figure.colorbar(im, ax=ax)
+            cbar.ax.set_ylabel(figure_options.z_axis_label, rotation=-90, va="bottom")
+
+        Heatmap.__set_figure_labels_title(ax=ax, datasets_map=datasets_map, figure_options=figure_options)
+
         plt.show()
 
     @staticmethod
-    def __build_single_dataset_value_map(data_from_db: list,
-                                         data_to_x_axis,
-                                         data_to_y_axis,
-                                         data_as_map_value,
-                                         mapping_fun=None) -> MultipleDatasetsValuesMap:
+    def __set_figure_labels_title(ax, datasets_map: MultipleDatasetsValuesMap, figure_options: FigureOptions):
+        ax.set_xticks(np.arange(len(datasets_map.x_labels)), labels=datasets_map.x_labels)
+        ax.set_yticks(np.arange(len(datasets_map.y_labels)), labels=datasets_map.y_labels)
+
+        ax.set_xlabel(figure_options.x_axis_label)
+        ax.set_ylabel(figure_options.y_axis_label)
+        ax.set_title(figure_options.title)
+
+    @staticmethod
+    def __set_annotations_to_heatmap(ax, heatmap_values, to_percentage=False):
+        divisor = 1
+        if to_percentage:
+            divisor = max(map(max, heatmap_values))
+
+        for y in np.arange(len(heatmap_values)):
+            for x in np.arange(len(heatmap_values[0])):
+                ax.text(x, y, round(heatmap_values[y, x] / divisor, 2), ha="center", va="center", color="w")
+
+    @staticmethod
+    def __build_value_map(data_from_db: list,
+                          data_to_x_axis,
+                          data_to_y_axis,
+                          data_as_map_value,
+                          mapping_fun=None) -> MultipleDatasetsValuesMap:
         data_as_points = []
 
+        if mapping_fun is None:
+            mapping_fun = lambda x: x
+
         for d in data_from_db:
-
-            if mapping_fun is None:
-                point = Single3DPoint(x=float(d[data_to_x_axis]),
-                                      y=float(d[data_to_y_axis]),
-                                      z=float(d[data_as_map_value]))
-            else:
-                point = Single3DPoint(x=float(d[data_to_x_axis]),
-                                      y=float(d[data_to_y_axis]),
-                                      z=float(mapping_fun(d[data_as_map_value])))
-
+            point = Single3DPoint(x=float(d[data_to_x_axis]),
+                                  y=float(d[data_to_y_axis]),
+                                  z=float(mapping_fun(d[data_as_map_value])))
             data_as_points.append(point)
 
         x_labels = list(map(lambda x: x[data_to_x_axis], data_from_db))
--- examples/heatmap.py
+++ examples/heatmap.py
@@ -1,12 +1,11 @@
-import statistics
-
 from application_management.app import AppManager
+from data_visualisation._implementations.heatmap import HeatmapConfig
 from data_visualisation.models import FigureOptions
 from consts.system_util import PATH_TO_MAIN_FOLDER
 from data_visualisation.plotting_facade import PlottingFacade
 
-object_to_plot = 'cube'
-dataset_to_plot = 'white_only'
+object_to_plot = 'cone'
+dataset_for_objects = 'manual'  # manual white_noise white_only
 
 plotted_param = 'standard_deviation_of_processed_image'
 x_axis = "x"
@@ -14,25 +13,28 @@ y_axis = "z"
 
 app_manager = AppManager()
 app_manager.set_main_folder(PATH_TO_MAIN_FOLDER)
-app_manager.set_object(object='sphere')
+app_manager.set_object(object=object_to_plot)
 
 data_from_db = (app_manager
                 .load_data_from_db()
-                .custom_data({}))
+                .custom_data({"dataset": dataset_for_objects}))
 
 app_manager.set_data_from_db(data_from_db=data_from_db)
 
 figure_options = FigureOptions(
     x_axis_label=x_axis,
     y_axis_label=y_axis,
-    z_axis_label=plotted_param
+    z_axis_label=plotted_param,
+    title=f'{object_to_plot}: {plotted_param}'
 )
 
+heatmap_config = HeatmapConfig(
+    show_cbar=False,
+    show_annotations=True
+)
 
-def mapping_fun(data):
-    if data is None:
-        return 0
-    return statistics.stdev(data[2:])
-
-
-PlottingFacade.heatmap().plot_data(data_from_db=data_from_db, figure_options=figure_options)
+PlottingFacade.heatmap().plot_data(
+    data_from_db=data_from_db,
+    figure_options=figure_options,
+    config=heatmap_config
+)
